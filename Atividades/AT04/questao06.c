4474483748ef04378232ce23f8e97aa05518ae64
Lucas-Bispo <lukaobispo@gmail.com>
2021-04-01 20:11:22 -0300

    Questão 05

---
+++ Atividades/AT04/questao06.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void ler3Letras(){
+    char l1,l2,l3;
+
+    printf("Digite seu L1: ");
+    scanf("%c",&l1);
+
+    printf("Digite seu L2: ");
+    scanf("%c",&l1);
+
+    printf("Digite seu L2: ");
+    scanf("%c",&l3);
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    ler3Letras();
+
+    
+    return 0;
+}
