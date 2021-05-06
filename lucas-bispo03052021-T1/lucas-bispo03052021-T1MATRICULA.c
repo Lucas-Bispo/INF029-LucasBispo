c166acefc071ab238cf7cb8ae10930e8e3ddddbb
Lucas-Bispo <lukaobispo@gmail.com>
2021-05-06 08:51:19 -0300

    Atualização de cconteudo

--- lucas-bispo03052021-T1/lucas-bispo03052021-T1MATRICULA.c
+++ lucas-bispo03052021-T1/lucas-bispo03052021-T1MATRICULA.c
@@ -22,17 +22,22 @@
 // #################################################
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <ctype.h>
+#include "lucas-bispo03052021-T1MATRICULA.h"
 
 /*
-## função utilizada para testes  ##
- somar = somar dois valores
-@objetivo
+	## função utilizada para testes  ##
+	
+	somar = somar dois valores
+	@objetivo
     Somar dois valores x e y e retonar o resultado da soma
-@entrada
+	@entrada
     dois inteiros x e y
-@saida
+	@saida
     resultado da soma (x + y)
- */
+*/
 int somar(int x, int y)
 {
     int soma = 0;
@@ -41,179 +46,386 @@ int somar(int x, int y)
 }
 
 /*
-## função utilizada para testes  ##
- fatorial = fatorial de um número
-@objetivo
+	## função utilizada para testes  ##
+	
+	fatorial = fatorial de um número
+	@objetivo
     calcular o fatorial de um número
-@entrada
+	@entrada
     um inteiro x
-@saida
+	@saida
     fatorial de x -> x!
- */
+*/
 int fatorial(int x)
 { //função utilizada para testes
     int fat = 1;
     return fat;
 }
 
-int teste(int a)
-{
-    int val;
-    if (a == 2)
-        val = 3;
-    else
-        val = 4;
-
-    return val;
-}
-
-/*typedef struct DQ
-{
-    char sDia [3];
-    char sMes [3];
-    char sAno [3];
-    int valido; // 0 se inválido, e 1 se válido 
-} DataQuebrada;
-DataQuebrada quebraData(char *data){
-    //[ 1 , 2  ,  /  ,    ,   ,  /  ,    ,     ,    ,      ,]
-}*/
 /*
- Q1 = validar data
-@objetivo
+	Q1 = validar data
+	@objetivo
     Validar uma data
-@entrada
+	@entrada
     uma string data. Formatos que devem ser aceitos: dd/mm/aaaa, onde dd = dia, mm = mês, e aaaa, igual ao ano. dd em mm podem ter apenas um digito, e aaaa podem ter apenas dois digitos.
-@saida
+	@saida
     0 -> se data inválida
     1 -> se data válida
- @restrições
+	@restrições
     Não utilizar funções próprias de string (ex: strtok)   
     pode utilizar strlen para pegar o tamanho da string
- */
+*/
 int q1(char *data)
 {
-    int datavalida = 1;
-
+	
+    
+    
+	
+    int k;
+    int l;
+	
+    
+	
     //quebrar a string data em strings sDia, sMes, sAno
-
-    //DataQuebrada dataQuebrada = quebraData(data);
-    //if (dataQuebrada.valido == 0) return 0;
-
+	int datavalida = 1;
+	
+    int mesf;
+    int diaf;
+    int anof;
+    int ndia;
+    int nmes;
+    int nano;
+	
+    //quebrar a string data em strings sDia, sMes, sAno
+	
+	
+	
+	
+	char sDia[3];
+	char sMes[3];
+	char sAno[5];
+	int i;
+	
+	for (i = 0; data[i] != '/'; i++){
+		sDia[i] = data[i];	
+	}
+	if(i == 1 || i == 2){ // testa se tem 1 ou dois digitos
+		sDia[i] = '\0';  // coloca o barra zero no final
+	}else 
+	return 0;
+	
+	
+	int j = i + 1; //anda 1 cada para pular a barra
+	i = 0;
+	for (; data[j] != '/'; j++){
+		sMes[i] = data[j];
+		i++;
+	}
+	
+	if(i == 1 || i == 2){ // testa se tem 1 ou dois digitos
+		sMes[i] = '\0';  // coloca o barra zero no final
+	}else
+	return 0;
+	
+	
+	j = j + 1; //anda 1 cada para pular a barra
+	i = 0;
+	
+	for(; data[j] != '\0'; j++){
+	 	sAno[i] = data[j];
+	 	i++;
+	}
+	
+	if(i == 2 || i == 4){ // testa se tem 2 ou 4 digitos
+		sAno[i] = '\0';  // coloca o barra zero no final
+	}else
+	return 0;
+	
+	
+	//imprimir os valores apenas para teste
+	printf("---------------");
+	printf("Data: %s\n", data);	
+	printf("Dia: %s\n", sDia);
+	printf("Mes: %s\n", sMes);
+	printf("Ano: %s\n", sAno);
+	
+	return 1; 
+	
+	
+	
+	
+	
+	
+	
+	
     //converter sDia, sMes e sAno em inteiros (ex: atoi)
-
+	diaf=atoi(sDia);
+	mesf=atoi(sMes);
+	anof=atoi(sAno);
+	
     //criar as variáveis iDia, iMes, iAno
-    int iAno = atoi(dataQuebrada.sAno);
-
-    //printf("%s\n", data);
-
-    if (datavalida)
-        return 1;
-    else
-        return 0;
+	
+    
+	
+    
+	
+    switch (mesf)
+    {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+		{
+			if (diaf < 1 || diaf > 31)
+			{
+				return 0;
+			}
+			break;
+		}
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+		{
+			if (diaf < 1 || diaf > 30)
+			{
+				return 0;
+			}
+			break;
+		}
+		case 2:
+		{
+			if (diaf == 29 && (anof/4==0))
+			{
+				
+				return 1;
+			}
+			else if (diaf < 1 || diaf > 28)
+			{
+				return 0;
+			}
+			break;
+		}
+        default:
+		return 0;
+	}
+	
+	
+	
+	//validacao do ano
+	if(anof<1000||anof>9999){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+	//ano bissexto
+	if((anof%100==1&&anof%4==0)||anof%400==0){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	//printf("%s\n", data);
+	
+	if (datavalida){
+		return 1;
+	}
+    
+	else
+    return 0;
 }
 
 /*
- Q2 = diferença entre duas datas
- @objetivo
+	Q2 = diferença entre duas datas
+	@objetivo
     Calcular a diferença em anos, meses e dias entre duas datas
- @entrada
+	@entrada
     uma string datainicial, uma string datafinal. Além disso, a função tem três parâmetros qtdDias, qtdMeses e qtdAnos. Esses três parâmetros devem ser utilizados para guardar os resultados dos cálculos. Na chamada da função deve passar o valor -1 para os três
- @saida
+	@saida
     1 -> cálculo de diferença realizado com sucesso
     2 -> datainicial inválida
     3 -> datafinal inválida
     4 -> datainicial > datafinal
- */
+*/
 int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtdAnos)
 {
-
+	
     //calcule os dados e armazene nas três variáveis a seguir
     int nDias, nMeses, nAnos;
-
+	
     if (q1(datainicial) == 0)
-        return 2;
-
+	return 2;
+	
     nDias = 4;
     nMeses = 10;
     nAnos = 2;
-
+	
     /*mantenha o código abaixo, para salvar os dados em 
-    nos parâmetros da funcao
-    */
+		nos parâmetros da funcao
+	*/
     *qtdDias = nDias;
     *qtdAnos = nAnos;
     *qtdMeses = nMeses;
-
+	
     //coloque o retorno correto
     return 1;
 }
-
+void converteTextoParaMaiuscula(char *texto)
+{
+    int i;
+    for (i = 0; texto[i] != '\0'; i++)
+        texto[i] = toupper(texto[i]);
+}
 /*
- Q3 = encontrar caracter em texto
- @objetivo
+	Q3 = encontrar caracter em texto
+	@objetivo
     Pesquisar quantas vezes um determinado caracter ocorre em um texto
- @entrada
+	@entrada
     uma string texto, um caracter c e um inteiro que informa se é uma pesquisa Case Sensitive ou não. Se isCaseSensitive = 1, a pesquisa deve considerar diferenças entre maiúsculos e minúsculos.
-        Se isCaseSensitive != 1, a pesquisa não deve  considerar diferenças entre maiúsculos e minúsculos.
- @saida
+	Se isCaseSensitive != 1, a pesquisa não deve  considerar diferenças entre maiúsculos e minúsculos.
+	@saida
     Um número n >= 0.
- */
+*/
 int q3(char *texto, char c, int isCaseSensitive)
 {
-    int qtdOcorrencias = -1;
+    int qtdOcorrencias = 0;
+    int i;
+
+    if (isCaseSensitive == 0)
+    {
+        converteTextoParaMaiuscula(texto);
+        c = toupper(c);
+    }
+
+    for (i = 0; texto[i] != '\0'; i++)
+    {
+        if (texto[i] == c)
+        {
+            qtdOcorrencias++;
+        }
+    }
 
     return qtdOcorrencias;
 }
 
+
+
+
 /*
- Q4 = encontrar palavra em texto
- @objetivo
+	Q4 = encontrar palavra em texto
+	@objetivo
     Pesquisar todas as ocorrências de uma palavra em um texto
- @entrada
+	@entrada
     uma string texto base (strTexto), uma string strBusca e um vetor de inteiros (posicoes) que irá guardar as posições de início e fim de cada ocorrência da palavra (strBusca) no texto base (texto).
- @saida
+	@saida
     Um número n >= 0 correspondente a quantidade de ocorrências encontradas.
     O vetor posicoes deve ser preenchido com cada entrada e saída correspondente. Por exemplo, se tiver uma única ocorrência, a posição 0 do vetor deve ser preenchido com o índice de início do texto, e na posição 1, deve ser preenchido com o índice de fim da ocorrencias. Se tiver duas ocorrências, a segunda ocorrência será amazenado nas posições 2 e 3, e assim consecutivamente. Suponha a string "Instituto Federal da Bahia", e palavra de busca "dera". Como há uma ocorrência da palavra de busca no texto, deve-se armazenar no vetor, da seguinte forma:
-        posicoes[0] = 13;
-        posicoes[1] = 16;
-        Observe que o índice da posição no texto deve começar ser contado a partir de 1.
-        O retorno da função, n, nesse caso seria 1;
- */
+	posicoes[0] = 13;
+	posicoes[1] = 16;
+	Observe que o índice da posição no texto deve começar ser contado a partir de 1.
+	O retorno da função, n, nesse caso seria 1;
+	
+*/
 int q4(char *strTexto, char *strBusca, int posicoes[30])
 {
     int qtdOcorrencias = -1;
-
+	
     return qtdOcorrencias;
 }
 
 /*
- Q5 = inverte número
- @objetivo
+	Q5 = inverte número
+	@objetivo
     Inverter número inteiro
- @entrada
+	@entrada
     uma int num.
- @saida
+	@saida
     Número invertido
- */
+*/
 
-int q5(int num)
-{
-
-    return num;
+int q5(int num ){
+	int inv=0;
+	int i=1;
+	while(i<=num)
+	{
+		inv*=10;
+		inv+=(num%(i*10)-num%i)/i;
+		i*=10;
+	}
+	
+	
+	return inv;
 }
-
 /*
- Q6 = ocorrência de um número em outro
- @objetivo
+	Q5 = ocorrência de um número em outro
+	@objetivo
     Verificar quantidade de vezes da ocorrência de um número em outro
- @entrada
+	@entrada
     Um número base (numerobase) e um número de busca (numerobusca).
- @saida
+	@saida
     Quantidade de vezes que número de busca ocorre em número base
- */
-
+*/
+/*
 int q6(int numerobase, int numerobusca)
-{
+{       
     int qtdOcorrencias;
-    return qtdOcorrencias;
+
+    if(numerobase==0){
+      return 0;
+    }
+    else if(numerobusca==(numerobase%10)){
+     qt
+      
+    }
+
+
+
+
+    return qtdOcorrencias;*/
+
+
 }
+
+
+ /*3 - Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um número natural N.
+ *  Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.
+#include<stdio.h>
+
+int numero;
+int aux=0;
+int k=0;
+
+int verifica(int n)
+{
+    if(n==0)
+      return 0;
+     else
+    {
+     if(aux==(n %10))
+     {
+        k++;
+     }
+     n=n / 10;
+     return verifica(n);
+    }
+return 0;
+}
+
+
+int main()
+{
+   printf("Digite um numero:");
+   scanf("%d",&numero);
+   printf("Digite o numero a ser verificado:");
+   scanf("%d",&aux);
+   verifica(numero);
+   printf("Vezes de numeros repetidos %d",k);
+   return 0;
+}
+
+
+*/
